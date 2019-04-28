7ac64c201d37647b818eca029ac0fd074134e00e
Keith Burgie <krburgie@gmail.com>
2019-04-28 23:16:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(args)
+  i = 0
+  
+  collection = []
+  puts "the program is executing the code inside the method"
+  
+  while i < args.length
+    yield(args[i])
+    collection << args[i]
+    i += 1
+  end
+  
+  collection
+end
 
-
+my_collect(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
