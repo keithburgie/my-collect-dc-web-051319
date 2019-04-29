fcb46565bf5e71c7d8629941dce5c89b7df19bb2
Keith Burgie <krburgie@gmail.com>
2019-04-29 00:53:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,3 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-
 def my_collect(array)
   newArray = []
   i = 0
@@ -10,18 +6,5 @@ def my_collect(array)
     newArray << array[i]
     i += 1
   end
-end
-
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-my_collect(languages) do |lang|
-  lang.upcase
-end
-
-my_collect(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
+  newArray
 end
